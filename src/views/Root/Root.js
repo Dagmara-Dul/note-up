8fcf9ccbe6410f5f5fe0c2e875c33cd789c6e0bc
Dag4 <dul.dagmara@gmail.com>
2021-05-31 15:05:12 +0200

    global styles, happy rems, antialiasing

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>Hello Daga</h1>
-    <Button width="500px">Close / Save</Button>
+    <Button>Close / Save</Button>
     <Button secondary>Remove</Button>
   </div>
 );
